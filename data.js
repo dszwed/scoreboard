eb8d33627a2384e63138376fdf0fe7f4c6fb5660
Dawid Szwed <dawid@gootek.pl>
2022-05-15 14:57:05 +0200

    Update players

--- data.js
+++ data.js
@@ -8,9 +8,9 @@ export const teams = [
         players: [
             new Player(1, 'Dawid Szwed', 9),
             new Player(4, 'Radek Piotrowski', 20),
-            new Player(5, 'Zbyszek Baguda', 17),
-            new Player(6, 'Tomasz Zając', 15),
-            new Player(7, 'Marek Drobisz', 0),
+            // new Player(5, 'Zbyszek Baguda', 17),
+            // new Player(6, 'Tomasz Zając', 15),
+            new Player(7, 'Marek Drobisz', 83),
         ]
     },
     {
@@ -18,10 +18,11 @@ export const teams = [
         name: 'Jorgusie',
         score: '0',
         players: [
-            new Player(2, 'Maciej Szczepańczyk', 6),
+            // new Player(2, 'Maciej Szczepańczyk', 6),
             new Player(31, 'Kamil Jabłoński', 9),
             new Player(32, 'Bartek Jabłoński', 69),
             new Player(33, 'Marek Olearczyk', 96),
+            new Player(33, 'Olek Olearczyk', 0),
         ]
     },
     {
@@ -32,8 +33,8 @@ export const teams = [
             new Player(10, 'Bogusz Siemek', 1),
             new Player(25, 'Wojtek Cetnarski', 14),
             new Player(26, 'Tomasz Karcz', 4),
-            new Player(27, 'Korneliusz Siemek', 0),
-            new Player(34, 'Szymon Bratus', 0),
+            new Player(27, 'Korneliusz Siemek', 2),
+            // new Player(34, 'Szymon Bratus', 0),
         ]
     },
     {
@@ -41,9 +42,9 @@ export const teams = [
         name: 'Banda Łysego',
         score: '0',
         players: [
-            new Player(11, 'Maciek Porwisz', 0),
-            new Player(28, 'Wiktor Papla', 0),
-            new Player(29, 'Karol Bożek', 0),
+            new Player(11, 'Maciek Porwisz', 28),
+            new Player(28, 'Wiktor Papla', 8),
+            new Player(29, 'Bartosz Wojtas', 13),
             new Player(30, 'Dawid Jagoda', 0),
         ]
     },
@@ -52,10 +53,10 @@ export const teams = [
         name: 'Kentuchy Bucks',
         score: '0',
         players: [
-            new Player(12, 'Łukasz Cinal', 0),
+            new Player(12, 'Łukasz Cinal', 23),
             new Player(22, 'Krzysztof Szumowski', 0),
-            new Player(23, 'Przemysław Gacek', 0),
-            new Player(24, 'Marcin Jarosz', 0),
+            new Player(23, 'Przemysław Gacek', 5),
+            new Player(24, 'Marcin Jarosz', 21),
         ]
     },
     {
@@ -67,7 +68,7 @@ export const teams = [
             new Player(18, 'Tomasz Figura', 0),
             new Player(19, 'Piotr Stolarczyk', 0),
             new Player(20, 'Krzysiek Kopacz', 24),
-            new Player(21, 'Szymon Moskal', 24),
+            // new Player(21, 'Szymon Moskal', 24),
         ]
     },
     {
@@ -75,10 +76,10 @@ export const teams = [
         name: 'The Brickers',
         score: '0',
         players: [
-            new Player(14, 'Szymon Klaja', 0),
+            // new Player(14, 'Szymon Klaja', 0),
             new Player(15, 'Szymon Kolęda', 0),
             new Player(16, 'Bartek Klaja', 0),
-            new Player(17, 'Franek Stachura', 0),
+            new Player(17, 'Franek Stachura', 91),
         ]
     },
     {
